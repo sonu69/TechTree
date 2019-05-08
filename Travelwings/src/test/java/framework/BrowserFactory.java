26840df44ad064667d8d1fedd3df913365493578
sonu69 <sonutherealone@gmail.com>
2019-05-08 19:04:29 +0530

    success

---
+++ Travelwings/src/test/java/framework/BrowserFactory.java
@@ -0,0 +1,38 @@
+package framework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BrowserFactory {
+	
+private static WebDriver driver;
+	
+public static WebDriver openBrowser() {
+	String browsername = "firefox";
+	
+	if(browsername.equalsIgnoreCase("chrome")) {
+	System.setProperty("webdriver.chrome.driver", ".//Drivers//chromedriver");
+	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver");
+	
+	ChromeOptions options = new ChromeOptions();
+	options.setHeadless(true);
+	
+	driver = new ChromeDriver(options);
+		
+	} else if(browsername.equalsIgnoreCase("firefox")) {
+		System.setProperty("webdriver.gecko.driver", ".//Drivers//geckodriver");
+
+		FirefoxOptions options = new FirefoxOptions();
+		options.setHeadless(true);
+		
+		driver = new FirefoxDriver(options);
+	}
+	
+	return driver;
+}
+	
+
+}
--- Travelwings/src/test/java/framework/Flights.java
+++ Travelwings/src/test/java/framework/Flights.java
@@ -1,23 +1,44 @@
 package framework;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Flights {
 
+	WebDriver driver;
+	public ExtentReports report;
+	public ExtentTest logger;
+	
+	@BeforeClass
+	public void bc() {
+		driver=BrowserFactory.openBrowser();	 
+	}
+	
+	@BeforeSuite
+	public void setupsuite() {
+		//ExtentHtmlReporter extent = new ExtentHtmlReporter(new File(System.getProperty("user.dir")+"/Reports/sonu"+Helper.getCurrentDateTime()+".html"));
+		ExtentHtmlReporter extent = new ExtentHtmlReporter(new File(System.getProperty("user.dir")+"/Reports/sonu"+".html"));
+		report = new ExtentReports();
+		report.attachReporter(extent);
+	}
+	
 	@Test
 	public void flight() {	
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
-		//System.setProperty("webdriver.chrome.driver", ".//chromedriver");
-		ChromeOptions options = new ChromeOptions();  
-		//options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-		options.setHeadless(false);
+		logger=report.createTest("Flight booking");	 
+		logger.info("starting application");  
 		
-		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.get("https://newuat.travelwings.com/");
-		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
 	
