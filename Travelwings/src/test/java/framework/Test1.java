c88cb76fb2f8fe6a7285694a1fed7b947ecf3fae
sonu69 <sonutherealone@gmail.com>
2019-07-05 11:24:39 +0530

    success

--- Travelwings/src/test/java/framework/Test1.java
+++ Travelwings/src/test/java/framework/Test1.java
@@ -1,14 +1,6 @@
 package framework;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
-import actions.FlightSelect_Page;
 import actions.Search_Inputs;
 import pageObjects.SearchFlight_Page;
 import utils.Constants;
@@ -20,9 +12,7 @@ public class Test1 {
 		int TestCase_Row=ExcelUtils.getRowContains(Constants.testcase_name);
 		
 		WebDriver driver = BrowserFactory.openBrowser(TestCase_Row);
-		
-		
-	
+
 		Search_Inputs.add_origin(driver,TestCase_Row);
 		
 		Search_Inputs.add_destination(driver,TestCase_Row);
@@ -38,9 +28,6 @@ public class Test1 {
 		SearchFlight_Page.search_flight(driver).click();
 		
 		Thread.sleep(10000);
-		
-		
-	
-		
+			
 	}
 }
